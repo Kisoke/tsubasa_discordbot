d3d0054060dc9c608653da22fce45cf8383f96ba
gbasile001 <gaetan.basile.1@etu.u-bordeaux.fr>
2018-05-09 13:31:35 +0900

    Refacto of most of the code in the music command; major bugs are fixed.

--- tsubasa_commands/music.js
+++ tsubasa_commands/music.js
@@ -1,80 +1,178 @@
 const ytdl = require('ytdl-core');
 let playlist = {};
 
+/**
+ * Represents a song in playlists.
+ */
+class Song {
+    constructor(url, title) {
+        this.url = url;
+        this.title = title;
+    }
+}
+
+/**
+ * Class for playing {@link Song}s. One per server.
+ */
+class MusicPlayer {
+    /**
+     * Creates a new instance of a {@link MusicPlayer}.
+     * @param textChannel the textChannel in which logs are to be posted
+     * @param voiceChannel the voiceChannel in which the audio is to be played
+     */
+    constructor(textChannel, voiceChannel) {
+        this.textChannel = textChannel;
+        this.playing = false;
+        this.playlist = [];
+        this.dispatcher = null;
+        this.voiceChannel = voiceChannel;
+        this.currentSong = null;
+    }
+
+    /**
+     * Adds a song to this {@link MusicPlayer}'s playlist.
+     * @param song the song to add
+     */
+    addToPlaylist(song) {
+        if (song instanceof Song) {
+            this.playlist.push(song);
+            if (this.playing === true)
+                this.textChannel.send({
+                    embed: {
+                        color: 3447003,
+                        description: `Ajouté **${song.title}** à la playlist.`
+                    }
+                });
+        }
+    }
+
+    /**
+     * Retrieves a song from Youtube. If valid, adds it to the playlist, and if playlist was empty, starts the playback.
+     * @param url the youtube URL
+     */
+    retrieveSong(url) {
+        if (url === '' || url === undefined)
+            this.textChannel.send("Utilisation : !music play *lien Youtube*");
+        else {
+            ytdl.getInfo(url, (err, info) => {
+                if (err || info === undefined) {
+                    this.textChannel.send({
+                        embed: {
+                            color: 3447003,
+                            title: `Erreur pendant le chargement de la vidéo`,
+                            description: `*${err}*`
+                        }
+                    });
+                }
+                else {
+                    let toAdd = new Song(url, info.title);
+                    this.addToPlaylist(toAdd);
+                    if (this.playing === false) {
+                        this.startPlaying();
+                    }
+                }
+            });
+        }
+    }
+
+    /**
+     * Starts playing the contents of the playlist.
+     */
+    startPlaying() {
+        this.playing = true;
+        this.currentSong = this.playlist.shift();
+        if (this.currentSong !== undefined) {
+            this.voiceChannel.join().then(connection => {
+                this.playStream(connection);
+            }).catch(console.error);
+        }
+    }
+
+    playStream(connection) {
+        this.dispatcher = connection.playStream(ytdl(this.currentSong.url, {filter: "audioonly"})).on("start", () => {
+            this.textChannel.send({
+                embed: {
+                    color: 3447003,
+                    description: `Lecture de **${this.currentSong.title}**`
+                }
+            });
+        });
+        this.dispatcher.on("end", () => {
+            this.currentSong = this.playlist.shift();
+            if ((this.currentSong === undefined) && (this.playing === true)) {
+                this.playing = false;
+                this.voiceChannel.leave();
+                this.textChannel.send("Playlist vide, sortie du channel audio.");
+            }
+            else {
+                this.playStream(connection);
+            }
+        });
+    }
+
+    /**
+     * Skips the currently playing song.
+     */
+    skipSong() {
+        if (this.dispatcher !== undefined) {
+            this.textChannel.send({
+                embed: {
+                    color: 3447003,
+                    description: `Skipped ${this.currentSong.title}`,
+                }
+            });
+            this.dispatcher.end();
+        }
+    }
+
+
+    /**
+     * Stops playing, exits the voice channel and empties the playlist.s
+     */
+    stopPlaying() {
+        if (this.playing === false) this.textChannel.send({
+            embed: {
+                color: 3447003,
+                description: "La playlist est déjà vide."
+            }
+        });
+        else {
+            if (this.dispatcher !== undefined) {
+                this.dispatcher.end();
+                this.voiceChannel.leave();
+                this.playlist = [];
+            }
+        }
+    }
+}
+
 module.exports.run = (client, message, args) => {
     const server = message.guild;
     const sender = message.member;
+    let player = playlist[server.id];
 
-    if (!(playlist.hasOwnProperty(server.id))) {
-        playlist[server.id] = {};
-        playlist[server.id].playing = false;
-        playlist[server.id].songs = [];
-        playlist[server.id].dispatcher;
-        playlist[server.id].voiceChannel;
+    if (sender.voiceChannel !== undefined) {
+        if (!(playlist.hasOwnProperty(server.id))) {
+            playlist[server.id] = new MusicPlayer(message.channel, sender.voiceChannel);
+            player = playlist[server.id];
+        }
+        switch (args[0].toLowerCase()) {
+            case "play":
+                let url = args[1];
+                player.retrieveSong(url);
+                break;
+            case "stop":
+                player.stopPlaying();
+                break;
+            case "skip":
+                player.skipSong();
+                break;
+            default:
+                break;
+        }
     }
-
-    switch (args[0].toLowerCase()) {
-        case "play":
-            let validLink = true;
-            let url = args[1];
-            if (url === '' || url === undefined)
-                return message.channel.send("Utilisation : !music play *lien Youtube*");
-            ytdl.getInfo(url, (err, info) => {
-                if (err || info === undefined) {
-                    message.channel.send("Erreur pendant le chargement de la vidéo (*" + err + "*).");
-                    validLink = false;
-                }
-                else if (playlist[server.id].songs.length === 0)
-                    return message.channel.send(`Lecture de **${info.title}**`);
-                else return message.channel.send(`Ajouté **${info.title}** à la playlist.`);
-            });
-            if (validLink === true) {
-                if (!playlist[server.id].playing) {
-                    // Jouer l'audio de la video YT
-                    playlist[server.id].playing = true;
-                    sender.voiceChannel.join().then(connection => {
-                        playlist[server.id].voiceChannel = sender.voiceChannel;
-                        playlist[server.id].dispatcher = connection.playStream(ytdl(url, {filter: "audioonly"}));
-
-                        playlist[server.id].dispatcher.on("end", () => {
-                            url = playlist[server.id].songs.shift();
-                            if ((url === undefined) && (playlist[server.id].playing === true)) {
-                                playlist[server.id].playing = false;
-                                playlist[server.id].voiceChannel.leave();
-                                message.channel.send("Playlist vide, sortie du channel audio.");
-                            }
-                            else {
-                                playlist[server.id].dispatcher = connection.playStream(ytdl(url, {filter: "audioonly"}));
-                            }
-                        });
-                    }).catch(console.error);
-                }
-                else {
-                    // Ajouter a la playlist
-                    playlist[server.id].songs.push(url);
-                    console.log(playlist);
-                }
-            }
-            break;
-        case "stop":
-            if (playlist[server.id].playing === false) return message.channel.send({
-                embed: {
-                    color: 3447003,
-                    description: "La playlist est déjà vide."
-                }
-            });
-            else {
-                playlist[server.id].playing = false;
-                playlist[server.id].dispatcher.end();
-                playlist[server.id].voiceChannel.leave();
-                playlist[server.id].songs = [];
-                return;
-            }
-        case "skip":
-            playlist[server.id].dispatcher.end();
-            break;
-        default:
-            break;
+    else {
+        message.channel.send("Vous n'êtes pas dans un channel audio.");
     }
 };
 

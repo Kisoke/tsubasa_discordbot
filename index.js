411ace767f7de9da92ded144730ef42b66473805
gbasile001 <gaetan.basile.1@etu.u-bordeaux.fr>
2018-05-09 10:11:10 +0900

    Small fixes and renamed "play" command to "music"

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const Discord = require("discord.js");
 const fs = require("fs");
 const client = new Discord.Client();
-var config = require("./settings.json");
+const config = require("./settings.json");
 
 client.commands = new Discord.Collection();
 
 fs.readdir("./tsubasa_commands/", (err, files) => {
     if (err) return console.log(err);
-    let jsFiles = files.filter(filepath => filepath.split('.').pop() === "js");
+    let jsFiles = files.filter(filePath => filePath.split('.').pop() === "js");
     if (jsFiles.length <= 0) {
         console.log("No commands loaded.");
     } else {
         console.log(`${jsFiles.length} commands loaded.`);
-        jsFiles.forEach((file, i) => {
-            let cmds = require(`./tsubasa_commands/${file}`);
-            client.commands.set(cmds.help.command, cmds);
+        jsFiles.forEach((file) => {
+            let commands = require(`./tsubasa_commands/${file}`);
+            client.commands.set(commands.help.command, commands);
         });
     }
 });
@@ -32,7 +32,7 @@ client.on("message", function (message) {
     const cmd = message.content.toLowerCase().split(' ')[0].slice(config.prefix.length);
     const args = message.content.split(' ').slice(1);
 
-    var command = client.commands.get(cmd);
+    let command = client.commands.get(cmd);
     if (command) command.run(client, message, args);
 });
 
--- tsubasa_commands/music.js
+++ tsubasa_commands/music.js
@@ -1,5 +1,5 @@
 const ytdl = require('ytdl-core');
-var playlist = {};
+let playlist = {};
 
 module.exports.run = (client, message, args) => {
     const server = message.guild;
@@ -14,8 +14,50 @@ module.exports.run = (client, message, args) => {
     }
 
     switch (args[0].toLowerCase()) {
+        case "play":
+            let validLink = true;
+            let url = args[1];
+            if (url === '' || url === undefined)
+                return message.channel.send("Utilisation : !music play *lien Youtube*");
+            ytdl.getInfo(url, (err, info) => {
+                if (err || info === undefined) {
+                    message.channel.send("Erreur pendant le chargement de la vidéo (*" + err + "*).");
+                    validLink = false;
+                }
+                else if (playlist[server.id].songs.length === 0)
+                    return message.channel.send(`Lecture de **${info.title}**`);
+                else return message.channel.send(`Ajouté **${info.title}** à la playlist.`);
+            });
+            if (validLink === true) {
+                if (!playlist[server.id].playing) {
+                    // Jouer l'audio de la video YT
+                    playlist[server.id].playing = true;
+                    sender.voiceChannel.join().then(connection => {
+                        playlist[server.id].voiceChannel = sender.voiceChannel;
+                        playlist[server.id].dispatcher = connection.playStream(ytdl(url, {filter: "audioonly"}));
+
+                        playlist[server.id].dispatcher.on("end", () => {
+                            url = playlist[server.id].songs.shift();
+                            if ((url === undefined) && (playlist[server.id].playing === true)) {
+                                playlist[server.id].playing = false;
+                                playlist[server.id].voiceChannel.leave();
+                                message.channel.send("Playlist vide, sortie du channel audio.");
+                            }
+                            else {
+                                playlist[server.id].dispatcher = connection.playStream(ytdl(url, {filter: "audioonly"}));
+                            }
+                        });
+                    }).catch(console.error);
+                }
+                else {
+                    // Ajouter a la playlist
+                    playlist[server.id].songs.push(url);
+                    console.log(playlist);
+                }
+            }
+            break;
         case "stop":
-            if (playlist[server.id].playing == false) return message.channel.send({
+            if (playlist[server.id].playing === false) return message.channel.send({
                 embed: {
                     color: 3447003,
                     description: "La playlist est déjà vide."
@@ -28,56 +70,15 @@ module.exports.run = (client, message, args) => {
                 playlist[server.id].songs = [];
                 return;
             }
-            break;
         case "skip":
             playlist[server.id].dispatcher.end();
             break;
         default:
-            let validlink = true;
-            let url = args[0];
-            if (url == '' || url === undefined)
-                return message.channel.send("Utilisation : !play *lien Youtube*");
-            ytdl.getInfo(url, (err, info) => {
-                if (err | info === undefined) {
-                    message.channel.send("Lien Youtube invalide (*" + err + "*).");
-                    validlink = false;
-                }
-                else if (playlist[server.id].songs.length == 0)
-                    return message.channel.send(`Lecture de **${info.title}**`);
-                else return message.channel.send(`Ajouté **${info.title}** à la playlist.`);
-            });
-            if (validlink == true) {
-                if (!playlist[server.id].playing) {
-                    // Jouer l'audio de la video YT
-                    playlist[server.id].playing = true;
-                    sender.voiceChannel.join().then(connection => {
-                        playlist[server.id].voiceChannel = sender.voiceChannel;
-                        playlist[server.id].dispatcher = connection.playStream(ytdl(url, { filter: "audioonly" }));
-
-                        playlist[server.id].dispatcher.on("end", () => {
-                            url = playlist[server.id].songs.shift();
-                            if ((url === undefined) && (playlist[server.id].playing == true)) {
-                                playlist[server.id].playing = false;
-                                playlist[server.id].voiceChannel.leave();
-                                message.channel.send("Playlist vide, sortie du channel audio.");
-                            }
-                            else {
-                                playlist[server.id].dispatcher = connection.playStream(ytdl(url, { filter: "audioonly" }));
-                            }
-                        });
-                    }).catch(console.error);
-                }
-                else {
-                    // Ajouter a la playlist
-                    playlist[server.id].songs.push(url);
-                    console.log(playlist);
-                }
-            }
             break;
     }
-}
+};
 
 module.exports.help = {
-    name: "Play",
-    command: "play"
-}
+    name: "Music",
+    command: "music"
+};

9bd580c906299a975adfab21ead2e0093254cea1
gbasile001 <gaetan.basile.1@etu.u-bordeaux.fr>
2018-10-01 11:39:35 +0200

    Add autoreact command

---
+++ tsubasa_commands/autoReact.js
@@ -0,0 +1,36 @@
+let autoRTServers = {};
+const config = require("../settings.json");
+
+module.exports.run = (client, message, args) => {
+    const server = message.guild;
+    const sender = message.member;
+    if (!(autoRTServers.hasOwnProperty(server.id))) {
+        autoRTServers[server.id] = config.auto_react.default;
+    }
+    let toggle = args[0] ? args[0].toLowerCase() : "";
+    switch (toggle) {
+        case "off":
+            autoRTServers[server.id] = false;
+            message.react(`❎`);
+            break;
+        case "on":
+            autoRTServers[server.id] = true;
+            message.react(`✅`);
+            break;
+        default:
+            message.channel.send(`le paramètre doit être on/off`);
+            break;
+    }
+    client.on("message", function (message) {
+        if (autoRTServers[server.id] === true)
+            config.auto_react.emotes.forEach(emoteId => {
+                message.react(emoteId);
+            });
+        else return;
+    });
+}
+
+module.exports.help = {
+    name: "autoreact",
+    command: "autoreact"
+}
